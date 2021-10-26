092887cc14052a013f3ff2f8f78285e76b5ed6fe
Daniel Silveira <danielzinhors@gmail.com>
2021-10-26 20:32:21 -0300

    Ajustes no logo

--- src/components/Logo/test.tsx
+++ src/components/Logo/test.tsx
@@ -12,14 +12,14 @@ describe('<Logo />', () => {
 
   it('should render a normal logo when size is default', () => {
     renderWithTheme(<Logo />)
-    expect(screen.getByLabelText(/Eduick logo/i).parentElement).toHaveStyle({
+    expect(screen.getByLabelText(/Eduick logo/i)).toHaveStyle({
       width: '34.5rem'
     })
   })
 
   it('should render a bigger logo', () => {
     renderWithTheme(<Logo size="large" />)
-    expect(screen.getByLabelText(/Eduick logo/i).parentElement).toHaveStyle({
+    expect(screen.getByLabelText(/Eduick logo/i)).toHaveStyle({
       width: '66.79rem'
     })
   })
